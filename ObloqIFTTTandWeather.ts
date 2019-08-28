604e35dafaae495ca53dc537fcaffa02cf79fdcf
tangjie133 <1334875503@qq.com>
2019-08-28 13:10:33 +0800

    3

--- ObloqIFTTTandWeather.ts
+++ ObloqIFTTTandWeather.ts
@@ -94,7 +94,7 @@ enum LOCATION {
 /**
  *Obloq implementation method.
  */
-//% weight=10 color=#008B00 icon="\uf1eb" block="ObloqIFTTTandWeather"
+//% weight=10 color=#008B00 icon="\uf1eb" block="HTTP-MQTT-Weather"
 namespace ObloqIFTTTandWeather {
 
     //serial
